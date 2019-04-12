388b708b376e8ccbb6506b38ad4c69564513bdc5
Roman Kislov <roman@kislovs.ru>
2019-04-12 14:40:08 +0500

    Handlebars

--- src/app.js
+++ src/app.js
@@ -1,12 +1,40 @@
 const path = require("path");
 const express = require("express");
+const hbs = require('hbs')
 
 const app = express();
 
+// Difen paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname,"../templates");
 
+//Setip Handlebars engine and Views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
+
+app.get('', (req,res)=>{
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Roman Kislov'
+  })
+})
+app.get('/about', (req,res)=>{
+  res.render('about', {
+    title: 'About Me',
+    name: 'Roman Kislov'
+  })
+})
+app.get('/help', (req,res)=>{
+  res.render('help', {
+    title: 'Help Page',
+    name: "roman Kislov",
+    message: 'Help message'
+  })
+})
 app.get("/weather", (req, res) => {
   res.send({
     forecast: "it is snowing",
