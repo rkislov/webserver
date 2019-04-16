98e290b8c1959a4bf476b3823626e812ef9f7f0a
Roman Kislov <roman@kislovs.ru>
2019-04-16 13:12:13 +0500

    last commit of weather app

--- public/js/app.js
+++ public/js/app.js
@@ -2,19 +2,28 @@ console.log('Client side javascript file loaded')
 
 const weatherForm = document.querySelector('form')
 const searchElement = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+//messageOne.textContent = 'From javascript'
+
 
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault()
 
     const location = searchElement.value
 
+    messageOne.textContent = 'Loading....'
+    messageTwo.textContent = ''
+
     fetch('http://localhost:3000/weather?address=' + location).then((response)=>{
         response.json().then((data)=>{
             if (data.error) {
-                console.log(data.error)
+                messageOne.textContent = data.error
             } else {
-                console.log(data.location)
-                console.log(data.forecast)
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
             }
 
         })
